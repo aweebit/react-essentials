2be28a93fdfbf0ede10054287449138761afc237
Wee Bit <aweebit64@gmail.com>
2025-06-01 15:54:01 +0200

    Add useEventListener

---
+++ lib/hooks/useEventListener.ts
@@ -0,0 +1,106 @@
+/**
+ * @file Based on {@link https://github.com/juliencrn/usehooks-ts}
+ *
+ * @license MIT
+ *
+ * Copyright (c) 2020 Julien CARON
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import { useEffect, useLayoutEffect, useRef } from 'react';
+
+/**
+ * Adds `handler` as a listener for the event `eventName` of `element`
+ * (or `window` by default) with the provided `options` applied.
+ *
+ * It is the user's responsibility to make sure `element` and `options` values
+ * are correctly memoized!
+ */
+
+// SVGElement Event based useEventListener interface
+function useEventListener<
+  K extends keyof SVGElementEventMap,
+  T extends SVGElement,
+>(
+  eventName: K,
+  handler: (event: SVGElementEventMap[K]) => void,
+  element: T,
+  options?: boolean | AddEventListenerOptions,
+): void;
+
+// HTMLElement Event based useEventListener interface
+function useEventListener<
+  K extends keyof HTMLElementEventMap,
+  T extends HTMLElement,
+>(
+  eventName: K,
+  handler: (event: HTMLElementEventMap[K]) => void,
+  element: T,
+  options?: boolean | AddEventListenerOptions,
+): void;
+
+// Document Event based useEventListener interface
+function useEventListener<K extends keyof DocumentEventMap>(
+  eventName: K,
+  handler: (event: DocumentEventMap[K]) => void,
+  element: Document,
+  options?: boolean | AddEventListenerOptions,
+): void;
+
+// Window Event based useEventListener interface
+function useEventListener<K extends keyof WindowEventMap>(
+  eventName: K,
+  handler: (event: WindowEventMap[K]) => void,
+  element?: Window,
+  options?: boolean | AddEventListenerOptions,
+): void;
+
+function useEventListener(
+  eventName: string,
+  handler: (event: Event) => void,
+  element?: EventTarget,
+  options?: boolean | AddEventListenerOptions,
+) {
+  // Create a ref that stores handler
+  const savedHandler = useRef(handler);
+
+  useLayoutEffect(() => {
+    savedHandler.current = handler;
+  }, [handler]);
+
+  useEffect(() => {
+    // Define the listening target
+    const targetElement = element ?? window;
+
+    // Create event listener that calls handler function stored in ref
+    const listener: typeof handler = (event) => {
+      savedHandler.current(event);
+    };
+
+    targetElement.addEventListener(eventName, listener, options);
+
+    // Remove event listener on cleanup
+    return () => {
+      targetElement.removeEventListener(eventName, listener, options);
+    };
+  }, [eventName, element, options]);
+}
+
+export default useEventListener;
--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,3 @@
 export { default as useDerivedState } from './hooks/useDerivedState';
+export { default as useEventListener } from './hooks/useEventListener';
 export { default as useForceUpdate } from './hooks/useForceUpdate';
