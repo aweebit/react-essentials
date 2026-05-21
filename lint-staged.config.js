99a3f34b21869f9ec5f5da52951eb88f4ea019ed
Wee Bit <aweebit64@gmail.com>
2026-05-21 13:29:01 +0200

    Add doc generation & package-lock.js synchronization to lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,5 @@
 const pattern = '*.{js,jsx,ts,tsx}';
+const docCommands = [() => 'npm run doc', () => 'git add README.md'];
 
 /**
  * @type {import('lint-staged').Configuration}
@@ -8,8 +9,14 @@ const config = {
     'npm run prettier:base --',
     'npm run lint --',
     () => 'tsc -b --noEmit',
+    ...docCommands,
   ],
   [`!${pattern}`]: 'npm run prettier:base -- --ignore-unknown',
+  'package.json': [
+    () => 'npm i',
+    () => 'git add package-lock.json',
+    ...docCommands,
+  ],
 };
 
 export default config;
